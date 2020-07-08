e4b602d3bb1bb6b665f64a72d09ee6f436539466
seantyh <seantyh@gmail.com>
2020-07-09 00:38:00 +0800

    initial commit

---
+++ precache-manifest.e12ce0276dda33d37fdad110a908837b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1990cc28533cfa904c64c0e584f3e0",
+    "url": "/CwnSenseAnnot/index.html"
+  },
+  {
+    "revision": "94e572edebe850027fc7",
+    "url": "/CwnSenseAnnot/static/css/2.074064f9.chunk.css"
+  },
+  {
+    "revision": "ddfd3246322bab79133e",
+    "url": "/CwnSenseAnnot/static/css/main.f1965b7f.chunk.css"
+  },
+  {
+    "revision": "94e572edebe850027fc7",
+    "url": "/CwnSenseAnnot/static/js/2.2215e526.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/CwnSenseAnnot/static/js/2.2215e526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfd3246322bab79133e",
+    "url": "/CwnSenseAnnot/static/js/main.393f6925.chunk.js"
+  },
+  {
+    "revision": "61b0411bb2ded4710790",
+    "url": "/CwnSenseAnnot/static/js/runtime-main.b51d6eaf.js"
+  }
+]);
