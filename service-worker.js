92c99eb86979c670997ceaa5703626f96b790490
seantyh <seantyh@gmail.com>
2020-07-20 01:44:23 +0800

    update build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CwnSenseAnnot/precache-manifest.625a8833dd64cec1854fb3b2afae1117.js"
+  "/CwnSenseAnnot/precache-manifest.b0338a00625a08aeb31d29883b2c3730.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CwnSenseAnnot/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
